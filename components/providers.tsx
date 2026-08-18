d74041efa47a8469dc3281d6bc819068cb8213b9
Jmail Filho <jamilf.alien@gmail.com>
2026-08-18 10:14:28 -0300

    feat: refactor Providers component for improved readability and structure

--- components/providers.tsx
+++ components/providers.tsx
@@ -3,5 +3,20 @@
 import { Provider } from "@lyket/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider apiKey="pt_89cc199a4163149abcc63d6668992a">{children}</Provider>;
+  return (
+    <Provider 
+      apiKey="pt_89cc199a4163149abcc63d6668992a"
+      theme={{
+        colors: {
+          highlight:"rgba(198, 168, 115, 1)",
+          background: "rgba(198, 168, 115, .05)",
+          primary: "rgba(198, 168, 115, .2)",
+          icon:"rgba(198, 168, 115, 1)",
+          text:"rgba(198, 168, 115, 1)"
+        }
+      }}
+    >
+    {children}
+  </Provider>
+  );
 }
